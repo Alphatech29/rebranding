7fd21d4966e04316bbe54c7b69915babd6471727
Alphatech29 <alphatechmultimedia@gmail.com>
2023-05-17 07:11:33 +0100

    Update code in index.html, style.css, animate.js

---
+++ js/animate.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: "2500",
+  delay: 400,
+});
+
+ScrollReveal().reveal(".top2, .side-logo", { delay: 500, origin: "left" });
+ScrollReveal().reveal(".top3, .card1", { delay: 600, origin: "bottom" });
+ScrollReveal().reveal(".about-info", { delay: 700, origin: "right" });
+ScrollReveal().reveal(".alpha1", { delay: 400, origin: "left", interval:"300"});
