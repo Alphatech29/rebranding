c5fe8f84c82941c772fe9819f3b90c0aacabf91f
Alphatech29 <alphatechmultimedia@gmail.com>
2023-05-31 16:49:58 +0100

    add new code to html, style.css,animate.js,main.js

--- js/animate.js
+++ js/animate.js
@@ -6,10 +6,11 @@ ScrollReveal({
   delay: 400,
 });
 
-ScrollReveal().reveal(".top2, .side-logo, .top-clutch", { delay: 500, origin: "left" });
-ScrollReveal().reveal(".top3, .card1", { delay: 600, origin: "bottom" });
-ScrollReveal().reveal(".about-info, .clutch", { delay: 700, origin: "right" });
+ScrollReveal().reveal(".top2, .side-logo, .top-clutch, .text12", { delay: 500, origin: "left" });
+ScrollReveal().reveal(".top3, .card1, .box", { delay: 600, origin: "bottom" });
+ScrollReveal().reveal(".about-info, .clutch, .hero-image", { delay: 700, origin: "right" });
 ScrollReveal().reveal(".alpha1, .awards1", { delay: 400, origin: "left", interval:"300"});
+ScrollReveal().reveal(".box-tittle, .text13, .top1", {delay:500, origin:"top"});
 
 //This is Typed Text Animation Code
 const typed = new Typed('#typed-text', {
--- js/main.js
+++ js/main.js
@@ -5,4 +5,43 @@ window.addEventListener('scroll', function() {
     header.classList.toggle('scrolled', scrolled);
   });
 
+//Code for counting section
+  let valueDisplays = document.querySelectorAll(".num1");
+  let interval = 4000;
+  
+  valueDisplays.forEach(valueDisplay => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let setValue = startValue; 
+    let counter = setInterval(function(){
+      setValue += 1;
+      valueDisplay.textContent = setValue + "+";
+      if(setValue === endValue){ 
+        clearInterval(counter);
+      }
+    }, duration);
+  });
+  
+   // Code that update the years of founding
+   const currentYear = new Date().getFullYear();
+   let foundingYear = 2009;
+
+   function updateYearsSinceFounding() {
+     const yearsSinceFounding = currentYear - foundingYear;
+     document.getElementById("yearsSinceFounding").textContent = yearsSinceFounding;
+   }
+   updateYearsSinceFounding(); 
+   setInterval(() => {
+     foundingYear++;
+     updateYearsSinceFounding(); 
+   }, 1000 * 60 * 60 * 24 * 365);
+
+   function updateYearsSinceFounding() {
+    const yearsSinceFounding = currentYear - foundingYear;
+    const yearsText = yearsSinceFounding >= 0 ? `${yearsSinceFounding}+` : "";
+    document.getElementById("yearsSinceFounding").textContent = yearsText;
+  }
+  
+
   
